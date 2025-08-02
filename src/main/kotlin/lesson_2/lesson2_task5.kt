839d546c3c2aab6a0ce5c6ee9b8c5436f81e09f6
Ekaterina Tarakanova <ktarakanova@yandex.ru>
2025-08-02 20:11:46 +0500

    KS-2-5 fixes

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,10 +2,12 @@ package lesson_2
 
 import kotlin.math.pow
 
+const val HUNDRED_PERCENT: Int = 100
+
 fun main() {
     val currentValue: Int = 70000
-    val interestRate: Double = 0.167
+    val interestRate: Double = 16.7
     val time: Int = 20
-    val futureValue: Double = currentValue * (1 + interestRate).pow(time.toDouble())
+    val futureValue: Double = currentValue * (1 + interestRate / HUNDRED_PERCENT).pow(time.toDouble())
     println("%.3f".format(futureValue))
 }
