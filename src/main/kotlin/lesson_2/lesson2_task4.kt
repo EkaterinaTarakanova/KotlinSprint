96c207244118fa78e5766970fb73a7c076bca374
Ekaterina Tarakanova <ktarakanova@yandex.ru>
2025-08-02 19:58:37 +0500

    KS-2-4 fixes

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,11 +1,14 @@
 package lesson_2
 
+const val HUNDRED_PERCENT: Int = 100
+
 fun main() {
     val crystalOreAmount: Int = 7
     val ironOreAmount: Int = 11
-    val buff: Double = 0.2
-    val bonusCrystalOreAmount: Int = (crystalOreAmount * buff).toInt()
-    val bonusIronOreAmount: Int = (ironOreAmount * buff).toInt()
+    val buff: Int = 20
+    val bonusCrystalOreAmount: Int = (crystalOreAmount * buff / HUNDRED_PERCENT)
+    val bonusIronOreAmount: Int = (ironOreAmount * buff / HUNDRED_PERCENT)
+
     println("Кристаллическая руда - $bonusCrystalOreAmount")
     println("Железная руда - $bonusIronOreAmount")
 }
