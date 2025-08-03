687f4354b584d51525dfa372272ce137752ef53e
Ekaterina Tarakanova <ktarakanova@yandex.ru>
2025-08-03 14:03:37 +0500

    KS-3-4 task4

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+    var from: String = "E2"
+    var to: String = "E4"
+    var step: Int = 1
+
+    println("$from-$to;$step")
+
+    from = "D2"
+    to = "D3"
+    step = 2
+
+    println("$from-$to;$step")
+}
