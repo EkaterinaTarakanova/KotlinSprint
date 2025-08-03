a50d17bcd90fc2781f691038f2ff282b1a54dffd
Ekaterina Tarakanova <ktarakanova@yandex.ru>
2025-08-03 17:23:05 +0500

    KS-3-5 fixes

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,10 +2,6 @@ package lesson_3
 
 fun main() {
     val moveString: String = "D2-D4;0"
-    val from: String = moveString.substringBefore("-")
-    val remains: String = moveString.substringAfter("-")
-    val to: String = remains.substringBefore(";")
-    val step: Int = moveString.substringAfter(";").toInt()
-
+    val (from, to, step) = moveString.split("-", ";")
     println("$from\n$to\n$step")
 }
