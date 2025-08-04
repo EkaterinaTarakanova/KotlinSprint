3084817d2a7c5b7f2d30a0645bb6d33434d38129
Ekaterina Tarakanova <ktarakanova@yandex.ru>
2025-08-04 15:15:31 +0500

    KS-4-3 fixes

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,17 +1,17 @@
 package lesson_4
 
 const val REQUIRED_SUNNY_WEATHER = true
-const val REQUIRED_OPEN_TEND = true
+const val REQUIRED_OPEN_TENT = true
 const val REQUIRED_HUMIDITY = 20
-const val REQUIRED_TIME_OF_YEAR = "не зима"
+const val REQUIRED_TIME_OF_YEAR = "зима"
 
 fun main() {
-    val weatherIsSunny: Boolean = true
-    val isTendOpen: Boolean = true
+    val isSunnyWeather: Boolean = true
+    val isTentOpen: Boolean = true
     val currentHumidity: Int = 20
     val timeOfYear: String = "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? " +
-            "${weatherIsSunny == REQUIRED_SUNNY_WEATHER && isTendOpen == REQUIRED_OPEN_TEND 
-                    && currentHumidity == REQUIRED_HUMIDITY && timeOfYear.equals(REQUIRED_TIME_OF_YEAR)}")
+            "${isSunnyWeather == REQUIRED_SUNNY_WEATHER && isTentOpen == REQUIRED_OPEN_TENT && 
+                    currentHumidity == REQUIRED_HUMIDITY && !timeOfYear.equals(REQUIRED_TIME_OF_YEAR)}")
 }
