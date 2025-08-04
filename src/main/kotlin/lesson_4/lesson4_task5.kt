d8e8f53727c3abf30451cd22e1c9ba0b9af396ba
Ekaterina Tarakanova <ktarakanova@yandex.ru>
2025-08-04 19:20:20 +0500

    KS-4-5 new fixes

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -6,7 +6,7 @@ const val BOXES_OF_PROVISIONS = 50
 
 fun main() {
     println("Введите наличие повреждений (true/false):")
-    val hullDamage: Boolean = readln().toBoolean()
+    val isHullDamage: Boolean = readln().toBoolean()
 
     println("Введите количество членов экипажа")
     val currentCrewNumber: Int = readln().toInt()
@@ -15,12 +15,12 @@ fun main() {
     val currentBoxesOfProvisions: Int = readln().toInt()
 
     println("Введите благоприятность погодных условий (true/false):")
-    val weatherConditions: Boolean = readln().toBoolean()
+    val isWeatherFavorable: Boolean = readln().toBoolean()
 
-    val canSail = (hullDamage == false &&  currentCrewNumber in MIN_CREW_NUMBER..MAX_CREW_NUMBER
-            && currentBoxesOfProvisions > BOXES_OF_PROVISIONS)
-            || (hullDamage == true && currentCrewNumber == MAX_CREW_NUMBER
-            && weatherConditions == true && currentBoxesOfProvisions >= BOXES_OF_PROVISIONS)
+    val canSail = (!isHullDamage && currentCrewNumber in MIN_CREW_NUMBER..MAX_CREW_NUMBER &&
+            currentBoxesOfProvisions > BOXES_OF_PROVISIONS) ||
+            (isHullDamage && currentCrewNumber == MAX_CREW_NUMBER &&
+                    isWeatherFavorable == true && currentBoxesOfProvisions >= BOXES_OF_PROVISIONS)
 
     println("Корабль может отправиться в плавание: $canSail")
 }
