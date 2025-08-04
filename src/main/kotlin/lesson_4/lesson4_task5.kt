796d064018c02124e4840a5c8231b28560a11abb
Ekaterina Tarakanova <ktarakanova@yandex.ru>
2025-08-04 15:05:29 +0500

    KS-4-5 task5

---
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -0,0 +1,26 @@
+package lesson_4
+
+const val MIN_CREW_NUMBER = 55
+const val MAX_CREW_NUMBER = 70
+const val BOXES_OF_PROVISIONS = 50
+
+fun main() {
+    println("Введите наличие повреждений (true/false):")
+    val hullDamage: Boolean = readln().toBoolean()
+
+    println("Введите количество членов экипажа")
+    val currentCrewNumber: Int = readln().toInt()
+
+    println("Введите количество ящиков провизии")
+    val currentBoxesOfProvisions: Int = readln().toInt()
+
+    println("Введите благоприятность погодных условий (true/false):")
+    val weatherConditions: Boolean = readln().toBoolean()
+
+    val canSail = (hullDamage == false &&  currentCrewNumber in MIN_CREW_NUMBER..MAX_CREW_NUMBER
+            && currentBoxesOfProvisions > BOXES_OF_PROVISIONS)
+            || (hullDamage == true && currentCrewNumber == MAX_CREW_NUMBER
+            && weatherConditions == true && currentBoxesOfProvisions >= BOXES_OF_PROVISIONS)
+
+    println("Корабль может отправиться в плавание: $canSail")
+}
